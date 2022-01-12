30b6787c2b5c50ea91588578c26e0e292918243e
jrermi12 <it.ermias.asmare@gmail.com>
2022-01-12 22:14:32 +0300

    basic Configuration for plugin dev

--- wp-content/plugins/documenter/documenter.php
+++
@@ -1,9 +0,0 @@
-/**
-* Plugin Name: Documenter
-* Plugin URI: http://yourdomain.com
-* Description: Insert a brief description of what your plugin does here.
-* Version: 1.0.0
-* Author: Your Name
-* Author URI: http://yourdomain.com
-* License: GPL2
-*/
